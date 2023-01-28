82b8aae85a20332e805c7070e7b301b810868f03
Lauren Powers <laurenpowers@Laurens-Air.lan>
2023-01-28 15:13:24 -0500

    add back button

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ function App() {
   return (
     <>
       <h1 style={{ textAlign: "center" }}>Photo Gallery with Modal</h1>
-      <Photos />
+      <Photos />{" "}
+      <div className="back-button-div">
+        <a
+          href="https://react-portfolio-lauren-powers.netlify.app/features"
+          rel="noreferrer"
+        >
+          <button className="button-54">Back to Lil' Portfolio </button>{" "}
+        </a>{" "}
+      </div>
     </>
   );
 }
